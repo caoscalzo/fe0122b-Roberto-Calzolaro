57151245a78dfb783dc7c648f49dd0bb13fa88dd
unknown <fullmoodyalchemist@gmail.com>
2022-02-14 18:14:41 +0100

    es1 s4

---
+++ S4/g1/js/myscript.js
@@ -0,0 +1,8 @@
+function changeColor(){
+    document.getElementById('colore').style.color = '#fff';
+    document.getElementById("colore1").style.color = "red"
+}
+
+function somma (){
+    return "Alessia" + "Roberto"
+}
