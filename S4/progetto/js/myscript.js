0b76c49d058c4f55b361fc5702d8d860aa89326b
unknown <fullmoodyalchemist@gmail.com>
2022-02-18 11:39:39 +0100

    quasi finito

--- S4/progetto/js/myscript.js
+++ S4/progetto/js/myscript.js
@@ -1,4 +1,25 @@
 
+//funzione per disattivare ripetizione operatori
+
+var operazioneClick = false;
+
+function tasti(valore){
+
+    if (valore == "+" || valore == "-" || valore == "*" || valore == "/"){
+        if (operazioneClick == false) {
+            document.getElementById("display").value += valore;
+            operazioneClick = true;
+        }
+    }
+    if (Number.isInteger(valore)){
+        document.getElementById("display").value += valore;
+    }
+}
+
+
+
+
+//funzione calcolatrice
 function prendiDato(elemento){
     let display=document.getElementById("display")
     let numero = elemento.id
@@ -18,6 +39,8 @@ function totale(){
     let display = document.getElementById("display")
     display.value = eval(display.value)
 }
+
+
 
 
 //alert("CIao! Benvenuto sulla mia calcolatrice!");
